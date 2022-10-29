d7f0cb863c4becbcca951b319c6846628e045e37
“[Levy <103933738+levyCro@users.noreply.github.com>
2022-10-29 19:11:44 +0200

    Installign breeze and migrate message and conversation database with elequent

---
+++ app/Models/Conversation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'sender_id',
+        'receiver_id',
+        'last_time_message'
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function user()
+    {
+        return $this->BelongsTo(User::class);
+    }
+}
