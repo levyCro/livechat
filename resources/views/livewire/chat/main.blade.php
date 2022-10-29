3aaf94e872ed4f7d3dd300712cfc69e8a197c1fd
“[Levy <103933738+levyCro@users.noreply.github.com>
2022-10-29 19:38:13 +0200

    installing livewire and creating there components

---
+++ resources/views/livewire/chat/main.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Chat') }}
+        </h2>
+    </x-slot>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    <div class="chat_container">
+
+        <div class="chat_list_container">
+            
+            @livewire('chat.chat-list')
+        </div>
+
+        <div class="chat_box_container">
+
+            @livewire('chat.chatbox')
+
+            @livewire('chat.send-message')
+        </div>
+    </div>
+
+</div>
